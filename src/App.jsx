2e50429c7a246364472af02d3dcf30c12cd83491
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-10-20 00:33:14 -0300

    Página suporte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import NavBar from "./Components/NavBar/NavBar";
 import SobreNos from "./Components/sobrenos"; // Corrigido o nome do componente
+import Suporte from "./Components/suporte/Suporte";
 import './stylesheets/index.scss';
 
 export default function App() {
   return (
     <>
       <NavBar />
-      <SobreNos />
+      <Suporte />
     </>
   );
 }
