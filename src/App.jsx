ba782544b9927c0193f4b677323ce07d7b7bb633
jesse <nitchsx@gmail.com>
2024-10-20 05:28:05 -0300

    Atualizações da home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import NavBar from "./Components/NavBar/NavBar";
-import SobreNos from "./Components/sobrenos"; // Corrigido o nome do componente
-import Suporte from "./Components/suporte/Suporte";
+// import NavBar from "./Components/NavBar/NavBar";
+import Home from './Components/home/index'
+// import SobreNos from "./Components/sobrenos"; // Corrigido o nome do componente
+// import Suporte from "./Components/suporte/Suporte";
 import './stylesheets/index.scss';
 
 export default function App() {
   return (
     <>
-      <NavBar />
-      <SobreNos />
+      <Home/>
     </>
   );
 }
--- src/Components/sobrenos.jsx
+++ src/Components/sobrenos.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Link } from "react-router-dom";
 import "../stylesheets/sobrenos/sobrenos.scss";
 
 export default function sobrenos() {
