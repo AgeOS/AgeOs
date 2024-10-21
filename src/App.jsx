551ec62ac05d14e78b6c951c045862d8650e417b
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-10-20 23:42:58 -0300

    feito a página de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import NavBar from "./Components/NavBar/NavBar";
+// import NavBar from "./Components/NavBar/NavBar";
 // import Home from './Components/home/index'
 // import SobreNos from "./Components/sobrenos"; // Corrigido o nome do componente
-import Suporte from "./Components/suporte/Suporte";
+// import Suporte from "./Components/suporte/Suporte";
 // import './stylesheets/index.scss';
+import Login from "./Components/login/Login";
 
 export default function App() {
   return (
     <>
-    <NavBar />
-    <Suporte />
+      <Login />
     </>
   );
 }
---
+++ src/Components/login/Login.jsx
@@ -0,0 +1,43 @@
+import '../../stylesheets/login/Login.scss'
+import logo from '../../assets/login/logo.png'
+import google from '../../assets/login/google.png'
+import face from '../../assets/login/face.png'
+import hotmail from '../../assets/login/hotmail.png'
+
+function Login() {
+    return (
+        <section className='container_login'>
+            <div className='lado_foto'>
+                {/* <img src={login} alt="Dois idosos segurando um celular" /> */}
+            </div>
+            <div className='lado_login'>
+                <div className='dentro_login'>
+                    <div className='logo_ageOS'>
+                        <img src={logo} alt="logo AgeOS" />
+                    </div>
+                    <h1>Login</h1>
+                    <h2>Bem-vindo de volta</h2>
+                    <input type="text" placeholder='Endereço de e-mail' />
+                    <input type="text" placeholder='Senha' />
+
+                    <label className="checkbox-container">
+                        <input type="checkbox" />
+                        <p>Manter conectado</p>
+                    </label>
+                    <button>Login</button>
+                    <a href="#"><p>Esqueci minha senha</p></a>
+                    <div className='container_contas'>
+                        <p>Ou entrar usando: </p>
+                        <img src={google} alt="" />
+                        <img src={face} alt="" />
+                        <img src={hotmail} alt="" />
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+export default Login
