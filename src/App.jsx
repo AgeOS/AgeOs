09caa27f422361e581fa56a32aa0f85355ff2c91
jesse <nitchsx@gmail.com>
2024-10-22 00:22:03 -0300

    atualizações da home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 
 // import NavBar from "./Components/NavBar/NavBar";
-// import Home from './Components/home/index'
+import Home from './Components/home/index'
 // import SobreNos from "./Components/sobrenos"; // Corrigido o nome do componente
 // import Suporte from "./Components/suporte/Suporte";
 // import './stylesheets/index.scss';
 // import Login from "./Components/login/Login";
+
 //import Cadastro from "./Components/cadastro/Cadastro";
+
+// import Cadastro from "./Components/cadastro/Cadastro";
+
 // import Pagamento from "./Components/QrCode/qrcode"
-import Usuario from "./Components/usuario/Usuario";
-import Adquira from "./Components/Adquira/Adquira";
+// import Usuario from "./Components/usuario/Usuario";
+// import Adquira from "./Components/Adquira/Adquira";
 
 export default function App() {
   return (
     <>
-      <Adquira />
-      <Usuario />
+      <Home/>
+
     </>
   );
 }
--- src/Components/home/index.jsx
+++ src/Components/home/index.jsx
@@ -2,7 +2,8 @@ import "../../stylesheets/home/index.scss";
 import "../../stylesheets/home/medias.scss";
 import NavBar from "../NavBar/NavBar";
 import Carousel from "./Cards/Cards";
-import contatoIcon from "../../assets/home/contatos.svg";
+// import people from '../../assets/home/avaliacao.svg'
+import Avaliacoes from './Avaliacoes/Avaliacoes'
 
 export default function Home() {
   return (
@@ -60,12 +61,97 @@ export default function Home() {
           </p>
 
           <div className="icons">
-           
+            <img src="src\assets\home\icones.png" alt="" />
           </div>
 
           <img src="src\assets\home\apps-intuitivos.png" alt="" />
         </div>
+
+        <div className="atendimento">
+          <img src="src\assets\home\atendimento.png" alt="" />
+
+          <h1>Atendimento</h1>
+          <p>
+            Suporte rápido e humanizado,adaptando às necessidades dos nossos
+            clientes
+          </p>
+        </div>
       </div>
+
+      <div className="pacotes">
+        <h1>Adquira seu AgeOS</h1>
+        <p>Os planos ideais que cabem no seu bolso</p>
+
+        <div className="cards">
+          <div className="pacote-basico">
+            <h2>Pacote básico</h2>
+
+            <img src="src\assets\home\image-32.png" alt="" />
+            <p>AgeOS</p>
+
+            <ul>
+              <li>Sistema apenas para um aparelho</li>
+              <li>Valor fixo por sistema</li>
+              <li>Somente atualições estáveis</li>
+              <li>Segurança para phishing e golpes</li>
+              <li>Identificador de chamadas suspeitas</li>
+              <li>Segurança reforçada em apps financeiros</li>
+            </ul>
+
+            <button>R$69,99</button>
+          </div>
+
+          <div className="pacote-premium">
+           <div className="recomendado">
+              <p>Recomendado</p>
+            </div> 
+
+            <h2>Pacote Premium</h2>
+
+            <img src="src\assets\home\image-32.png" alt="" />
+            <p>AgeOS</p>
+
+            <ul>
+              <li>Sistema para cinco aparelhos</li>
+              <li>Valor promocional</li>
+              <li>Acesso antecipado a atualizações</li>
+              <li>Segurança para phishing e golpes</li>
+              <li>Identificador de chamadas suspeitas</li>
+              <li>Segurança reforçada em apps financeiros</li>
+              <li>Pacote personalizado</li>
+              <li>Suporte para backup</li>
+            </ul>
+
+            <button className="button-buy-primium">R$299,99</button>
+
+          </div>
+        </div>
+
+        <button className="duvidas">Tire suas dúvidas</button>
+      </div>
+
+      <Avaliacoes/>
+
+    {/* <div className="avaliacoes">
+     <div className="avaliacao"> */}
+        {/* <h1>O que as pessoas dizem sobre a AgeOS</h1>
+
+        <div className="pessoas">
+          <div className="cada-pessoa">
+
+            <img src={people} alt="" />
+            <p>Felipe</p>
+          
+          </div>
+            <img className="aspas" src="src\assets\home\image-36.png" alt="" />
+            <p>Estou muito satisfeito com o AgeOS. Instalei no celular da minha avó e a segurança do sistema a faz se sentir protegida contra golpes, o que me tranquiliza. O suporte ao cliente também é excelente, sempre pronto para ajudar. </p>
+            <img className="aspas" src="src\assets\home\image-35.png" alt="" />
+        </div> */}
+     {/* </div>
+    </div> */}
+
+      
+
     </>
   );
 }
