7352f3af7bd138d546d8487de29c5b41bcf1752b
jesse <nitchsx@gmail.com>
2024-10-23 21:57:03 -0300

    att na navbar e nas querys da home

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,13 @@
 // import React from 'react';
 import { Link } from "react-router-dom";
-// import "../../stylesheets/navbar/index.scss";
-// import "../../stylesheets/navbar/media.scss";
-import '../../../public/stylesheets/navbar/index.css'
-import '../../../public/stylesheets/navbar/media.css'
+import "../../stylesheets/navbar/index.scss";
+import "../../stylesheets/navbar/media.scss";
+// import '../../../public/stylesheets/navbar/index.css'
+// import '../../../public/stylesheets/navbar/media.css'
 
 //imgs
 
-import menuFechado from '../../assets/navbar/menu-fechado.png'
+// import menuFechado from '../../assets/navbar/menu-fechado.png'
 import menuRoxo from '../../assets/navbar/menu-roxo.png'
 import logo from '../../assets/navbar/logo-1.png'
 import theme from '../../assets/navbar/image-34.png'
@@ -27,10 +27,10 @@ export default function NavBar() {
             
             if (modal.style.display === "none" || modal.style.display === "") {
               modal.style.display = "flex";
-              img.src = {menuFechado}; // Novo src da imagem
+              img.src = 'src/assets/navbar/icons8-x-48.png'; // Novo src da imagem
             } else {
               modal.style.display = "none";
-              img.src = {menuRoxo}; // src original da imagem
+              img.src = 'src/assets/navbar/menu-roxo.png'; // src original da imagem
             }
           }}
           >
@@ -39,7 +39,9 @@ export default function NavBar() {
 
         <div className="modal-lateral">
           <div className="conteudo">
+            <Link to='/'>
             <img src={logo} alt="" />
+            </Link>
 
             <ul>
               <li>
@@ -61,13 +63,13 @@ export default function NavBar() {
                 <img src={theme} alt="" />
               </button>
 
-                <Link>
+                <Link to='/login'>
                <button className="login">
                  Login
                 </button>
                 </Link>
 
-              <Link>
+              <Link to='/cadastro'>
               <button className="cadastro">
               Cadastre-se
               </button>
@@ -114,7 +116,9 @@ export default function NavBar() {
         
         <nav className="nav-bar">
         <div className="logo">
-          <img src={logo} alt="" />
+          <Link to='/'>
+            <img src={logo} alt="" />
+          </Link>
         </div>
   
         <ul>
@@ -125,7 +129,7 @@ export default function NavBar() {
             <Link to='/sobrenos'>Sobre nós</Link>
           </li>
           <li>
-            <Link to='/adquira'>Adquira</Link> {/* Corrigido de '../' para '/adquira' */}
+            <Link to='/adquira'>Adquira</Link>
           </li>
           <li>
             <Link to='/suporte'>Suporte</Link>
