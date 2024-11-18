79a9d814218fa7d5df8dc61ebedb5f50e82f4fa3
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-11-18 09:54:00 -0300

    Desenvolvimento incial

---
+++ backend/AgeOSBackend/src/main/java/com/ageos/AgeOSBackend/AgeOsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ageos.AgeOSBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeOsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgeOsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/AgeOSBackend/src/test/java/com/ageos/AgeOSBackend/AgeOsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ageos.AgeOSBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeOsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
