afc8890e17438ea63e03de52bfb494a3dda3d51b
unasc1ment0 <yuriisilva1308@gmail.com>
2024-11-26 22:01:25 -0300

    att

--- src/Components/Adquira/Adquira.jsx
+++ src/Components/Adquira/Adquira.jsx
@@ -12,6 +12,7 @@ import instaIcon from "../../assets/footer/insta-branco.png";
 import faceIcon from "../../assets/footer/face-branco.png";
 import linkeIcon from "../../assets/footer/linke-branco.png";
 import logo from "../../assets/Adquira/logo.png";
+import veio2 from "../../assets/Adquira/veio2.png";
 
 // import '../../stylesheets/Adquira/Adquira.scss'
 
@@ -101,6 +102,7 @@ function Adquira() {
           </svg>
           <h5 className="h3_1">Navegação na Internet</h5>
           <p className="textao4">Com nosso antivírus e proteção contra links e sites suspeitos, seu familiar estará protegido contra ameaças virtuais, oferecendo segurança para garantir que a navegação online seja segura e tranquila.</p>
+          <img className="Veia" src={veia2} alt="idoso" />
         </section>
 
          
