c9ab3e86c70c367aa8aa568508f333bc9b15b980
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-12-07 18:11:04 -0300

    Adicionando configuração Cors

---
+++ backend/AgeOSBackend/src/main/java/com/ageos/AgeOSBackend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ageos.AgeOSBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // Substitua pelo endereço do seu front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
