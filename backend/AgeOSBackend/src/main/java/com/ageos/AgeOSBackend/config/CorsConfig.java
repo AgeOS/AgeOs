e0723b1e202d5edec41982274cd53d9aa4845d24
Jefferson Jesus <130853172+JeffersonJesusDev@users.noreply.github.com>
2024-12-08 13:45:08 -0300

    Subindo API

--- backend/AgeOSBackend/src/main/java/com/ageos/AgeOSBackend/config/CorsConfig.java
+++ backend/AgeOSBackend/src/main/java/com/ageos/AgeOSBackend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Substitua pelo endereço do seu front-end
+                        .allowedOrigins("http://localhost:3000", "https://age-os.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
